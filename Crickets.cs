8948dd88a86ea686fb5abea65c5591444f80cd6a
likemeawq <148704557+likemeawq@users.noreply.github.com>
2023-10-22 18:18:50 +0300

    Add files via upload

---
+++ Crickets.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    private void Update() {
+        SayCrickets();
+    }
+
+    public void SayCrickets() {
+        print("Crickets");
+    }
+}
